dc94ba81894221f53345cc9bfa967910c7b9337d
Richard Gill <richard@rgill.co.uk>
2020-04-27 18:54:31 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import _ from 'lodash'
+
+while (true) {
+  if (_.random(0, 10, true) > 9.99999) {
+    console.log('Running1!')
+    console.log({ ...{ hello: 'wolrd' } })
+  }
+}
---
+++ test/test.ts
@@ -0,0 +1,4 @@
+test('adds 1 + 2 to equal 3', () => {
+  const result: number = 1 + 2
+  expect(result).toBe(3)
+})
